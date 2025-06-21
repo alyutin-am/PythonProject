95fd9e57bac88c3237c3a138657a16a45eb47bbd
Александр Алютин <alexandr.alyutin@gmail.com>
2025-06-21 22:52:19 +0300

    created function "filter_by_state"

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,13 @@
-def filter_by_state(dict: list, state='EXECUTED') -> list:
+def filter_by_state(dictionaries: list, state='EXECUTED') -> list:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ соответствует указанному значению, по умолчанию 'EXECUTED'"""
+    new_data = []
+    for dictionary in dictionaries:
+        if state in dictionary["state"]:
+            new_data.append(dictionary)
+    return new_data
 
 
 def sort_by_date(sort_list: list, reverse=True) -> list:
     """Функция должна возвращать новый список, отсортированный по дате, имеет возможность 'реверса'"""
     pass
+
