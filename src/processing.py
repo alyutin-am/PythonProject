5aab16fd7433fab72684550f0e80806d16f5d681
Александр Алютин <alexandr.alyutin@gmail.com>
2025-06-22 15:52:49 +0300

    after check linters

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,8 @@
-def filter_by_state(dictionaries: list, state='EXECUTED') -> list:
-    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ соответствует указанному значению, по умолчанию 'EXECUTED'"""
+def filter_by_state(dictionaries: list, state: str = "EXECUTED") -> list:
+    """
+    Функция возвращает новый список словарей, содержащий только те словари,
+    у которых ключ соответствует указанному значению, по умолчанию 'EXECUTED'
+    """
     new_data = []
     for dictionary in dictionaries:
         if state in dictionary["state"]:
@@ -7,7 +10,10 @@ def filter_by_state(dictionaries: list, state='EXECUTED') -> list:
     return new_data
 
 
-def sort_by_date(unsort_date: list, sort=True) -> list:
-    """Функция должна возвращать новый список, отсортированный по дате, имеет возможность сортировки по возрастанию или убыванию"""
-    sort_date = sorted(unsort_date, key=lambda unsort_date: unsort_date['date'], reverse=sort)
+def sort_by_date(unsort_date: list, sort: bool = True) -> list:
+    """
+    Функция должна возвращать новый список, отсортированный по дате,
+    имеет возможность сортировки по возрастанию или убыванию
+    """
+    sort_date = sorted(unsort_date, key=lambda unsort_date: unsort_date["date"], reverse=sort)
     return sort_date
