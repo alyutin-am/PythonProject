bf149ceceaf6ed5b34ef1564832fc83cf8f51fc1
Александр Алютин <alexandr.alyutin@gmail.com>
2025-07-05 19:18:06 +0300

    upgrade tests, functions, created html-test-file

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,18 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция скрытия номера карты (замены части номера карты на '*')"""
-    if len(card_number) != 16 or card_number.isdigit() == False:
-        return "Неверный номер карты"
-    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    if isinstance(card_number, str):
+        if len(card_number) != 16 or card_number.isdigit() == False:
+            return "Неверный номер карты"
+        return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    else:
+        return "Неверный тип данных"
 
 
 def get_mask_account(mask_account: str) -> str:
     """Функция скрытия номера счета (замены части номера счета на '*')"""
-    if len(mask_account) != 20 or mask_account.isdigit() == False:
-        return "Неверный номер счета"
-    return f"**{mask_account[-4:]}"
+    if isinstance(mask_account, str):
+        if len(mask_account) != 20 or mask_account.isdigit() == False:
+            return "Неверный номер счета"
+        return f"**{mask_account[-4:]}"
+    else:
+        return "Неверный тип данных"
--- src/processing.py
+++ src/processing.py
@@ -3,12 +3,15 @@ def filter_by_state(dictionaries: list, state: str = "EXECUTED") -> list:
     Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ соответствует указанному значению, по умолчанию 'EXECUTED'
     """
-    if len(dictionaries) != 0:
-        new_data = []
-        for dictionary in dictionaries:
-            if state in dictionary["state"]:
-                new_data.append(dictionary)
-        return new_data
+    if isinstance(dictionaries, list) and isinstance(state, str):
+        if len(dictionaries) != 0:
+            new_data = []
+            for dictionary in dictionaries:
+                if state in dictionary["state"]:
+                    new_data.append(dictionary)
+            return new_data
+        else:
+            return "Введены не верные данные, пожалуйста, устраните ошибку и попробуйте снова"
     else:
         return "Введены не верные данные, пожалуйста, устраните ошибку и попробуйте снова"
 
@@ -18,8 +21,11 @@ def sort_by_date(dictionaries: list, sort: bool = True) -> list:
     Функция должна возвращать новый список, отсортированный по дате,
     имеет возможность сортировки по возрастанию или убыванию
     """
-    if len(dictionaries) != 0:
-       sort_dictionaries = sorted(dictionaries, key=lambda unsort_date: unsort_date["date"], reverse=sort)
-       return sort_dictionaries
+    if isinstance(dictionaries, list) and isinstance(sort, bool):
+        if len(dictionaries) != 0:
+           sort_dictionaries = sorted(dictionaries, key=lambda unsort_date: unsort_date["date"], reverse=sort)
+           return sort_dictionaries
+        else:
+            return "Введены не верные данные, пожалуйста, устраните ошибку и попробуйте снова"
     else:
         return "Введены не верные данные, пожалуйста, устраните ошибку и попробуйте снова"
