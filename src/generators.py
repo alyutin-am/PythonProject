64dfe53e8a8bc36f5c789aca2f280bac71d75de4
Alexandr Alyutin sPC <alexandr.alyutin@gmail.com>
2025-07-19 22:54:58 +0300

    created generators, function + black, flake8. mypy - hane problems

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-def filter_by_currency(transaction_dictionaries: list, currency: str ="USD") -> iter:
+def filter_by_currency(transaction_dictionaries: list, currency: str = "USD") -> iter:
     """
     Функция принимает на вход список словарей и возвращает итератор,
     который поочередно выдает транзакции, где валюта операции соответствует currency
@@ -8,3 +8,29 @@ def filter_by_currency(transaction_dictionaries: list, currency: str ="USD") -> 
             yield dictionaries
 
 
+def transaction_descriptions(transaction_descr: list[dict]) -> GeneratorExit[str, None, None]:
+    """Генератор принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
+    for transaction in transaction_descr:
+        result_transaction_descr = transaction.get("description")
+        yield result_transaction_descr
+
+
+def card_number_generator(start: int = 1, stop: int = 9999999999999999) -> GeneratorExit[str, None, None]:
+    """
+    Генератор выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты.
+    Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    """
+    for gen_num in range(start, stop):
+        len_numbers = 16 - len(str(gen_num))
+        if gen_num <= 16:
+            card_number_gen = ("0" * len_numbers) + str(gen_num)
+            card_number_gen = (
+                card_number_gen[:4]
+                + " "
+                + card_number_gen[4:8]
+                + " "
+                + card_number_gen[8:12]
+                + " "
+                + card_number_gen[-4:]
+            )
+            yield card_number_gen
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,2 +1,13 @@
 def test_generators():
     pass
+
+
+#@pytest.mark.parametrize
+#    ("description, expected", [
+ #   ("Перевод организации", [939719570, 594226727]),
+  #  ("Перевод со счета на счет", [142264268, 873106923]),
+   # ("Перевод с карты на карту", [895315941]),
+#])
+#def test_transaction_descriptions(transactions, description, expected):
+ #   result = transaction_descriptions(transactions, description)
+  #  assert [t["id"] for t in result] == expected
