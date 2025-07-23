12a0f8f9e93fe96e655d0cb3043bbe1e63e848b6
Александр Алютин <alexandr.alyutin@gmail.com>
2025-07-23 12:32:01 +0300

    updates generators.py, decorators.py not doing

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,7 @@
-def filter_by_currency(transaction_dictionaries, currency="USD"):
+from typing import Any, Generator, Iterator
+
+
+def filter_by_currency(transaction_dictionaries: list, currency: str = "USD") -> Iterator[None]:
     """
     Функция принимает на вход список словарей и возвращает итератор,
     который поочередно выдает транзакции, где валюта операции соответствует currency
@@ -8,14 +11,14 @@ def filter_by_currency(transaction_dictionaries, currency="USD"):
             yield dictionaries
 
 
-def transaction_descriptions(transaction_descr):
+def transaction_descriptions(transaction_descr: list) -> Generator[dict, Any, None]:
     """Генератор принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
     for transaction in transaction_descr:
         result_transaction_descr = transaction.get("description")
         yield result_transaction_descr
 
 
-def card_number_generator(start=1, stop=9999999999999999):
+def card_number_generator(start: int = 1, stop: int = 9999999999999999) -> Generator[str, Any, None]:
     """
     Генератор выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты.
     Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
