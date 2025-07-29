241eaf1f21f1d6c8ba54fe18cba9a2f30355ef8a
Александр Алютин <alexandr.alyutin@gmail.com>
2025-07-29 18:46:22 +0300

    started sum_transaction

--- src/external_api.py
+++ src/external_api.py
@@ -1,6 +1,15 @@
 import json
+import requests
 
+headers = {
+    "apikey" : "G1D01mi8bZDRstqfKCeK6thvUvTl37LU"
+}
 
+url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={code_of_currency}&amount={dict_amount}"
 
-def sum_transaction():
-    pass
+def sum_transaction(operations: dict) -> float:
+    dict_amount = operations["amount"]
+    if operations["code"] != "RUB":
+        code_of_currency = operations["code"]
+        try:
+            result = requests.request("convert", url, headers=headers, data=)
